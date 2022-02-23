af6a1ebf2e3f4e76fe97ad3d7e4ca63e486afc08
vonred5 <82859955+vonred5@users.noreply.github.com>
2022-02-23 11:22:26 +0600

    new

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './header.module.css';
 import logo from './logo.png';
 
 function Header() {
+  const [styleMenu, setDisplay] = useState('none');
+
+  function menuClick() {
+    if (styleMenu === 'none') {
+      setDisplay('flex');
+    } else {
+      setDisplay('none');
+    }
+  }
+
   return (
     <header>
       <div className={s.logo}>
@@ -11,11 +23,11 @@ function Header() {
           <img src={logo} alt="logo" />
         </NavLink>
       </div>
-      <div className={s.nav_button}>
+      <div className={s.nav_button} onClick={menuClick}>
         <div className={s.line} />
         <div className={s.line} />
         <div className={s.line} />
-        <div className={s.menu}>
+        <div className={s.menu} style={{ display: styleMenu }}>
           <NavLink to="/genres">Жанры</NavLink>
           <NavLink to="/categories">Категории</NavLink>
           <NavLink to="/releases">Релизы</NavLink>
